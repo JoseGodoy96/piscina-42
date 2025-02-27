7364e929e6f6d8e7874fb5290defb5f5e01c9352
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-02-27 17:36:58 +0100

    c03-ex00

---
+++ c04/ex00/ft_strlen.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 17:32:25 by unlucky           #+#    #+#             */
+/*   Updated: 2025/02/27 17:36:20 by unlucky          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	char	str[5] = "hola";
+
+	ft_strlen(str);
+	ft_putnbr(ft_strlen(str));
+	return (0);
+}
