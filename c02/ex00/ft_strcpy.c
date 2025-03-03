3e78a699f76df566172baa4e849aa4c957d467ce
JoseGodoy <josemariagodoy1996@gmail.com>
2025-03-03 12:43:19 +0100

    c02-ex00

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,10 +6,31 @@
 /*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 01:46:11 by unlucky           #+#    #+#             */
-/*   Updated: 2025/03/02 01:46:15 by unlucky          ###   ########.fr       */
+/*   Updated: 2025/03/03 12:41:52 by unlucky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
 
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char src[5] = "hola";
+	char dest[5] = "";
+
+	ft_strcpy(dest, src);
+	write(1, dest, 5);
+	return (0);
+}
