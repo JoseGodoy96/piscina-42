0df11e6b7a85e3989a86c1388bbde4275dad3ae7
JoseGodoy <josemariagodoy1996@gmail.com>
2025-03-03 13:12:09 +0100

    c02-ex04

--- c02/ex04/ft_str_is_lowercase.c
+++ c02/ex04/ft_str_is_lowercase.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jgodoy-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/13 18:10:53 by jgodoy-m          #+#    #+#             */
-/*   Updated: 2025/03/03 13:11:48 by unlucky          ###   ########.fr       */
+/*   Created: 2025/02/13 18:32:04 by jgodoy-m          #+#    #+#             */
+/*   Updated: 2025/03/03 13:11:25 by unlucky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_str_is_numeric(char *str)
+int	ft_str_is_lowercase(char *str)
 {
 	int	i;
 
@@ -21,7 +21,7 @@ int	ft_str_is_numeric(char *str)
 		return (1);
 	while (str[i] != '\0')
 	{
-		if (!(str[i] >= '0' && str[i] <= '9'))
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
 			return (0);
 		i++;
 	}
@@ -33,19 +33,19 @@ void	correccion(int n)
 	char	c;
 
 	if (n >= 10)
-		correccion (n / 10);
+		correccion(n / 10);
 	c = (n % 10) + '0';
 	write(1, &c, 1);
 }
 
 int	main(void)
 {
-	char	str[] = "123";
-	char	str1[] = "asb123";
+	char	str[] = "abc";
+	char	str1[] = "abcABC";
 	char	str2[] = "";
 
-	correccion(ft_str_is_numeric(str));
-	correccion(ft_str_is_numeric(str1));
-	correccion(ft_str_is_numeric(str2));
+	correccion(ft_str_is_lowercase(str));
+	correccion(ft_str_is_lowercase(str1));
+	correccion(ft_str_is_lowercase(str2));
 	return (0);
 }
