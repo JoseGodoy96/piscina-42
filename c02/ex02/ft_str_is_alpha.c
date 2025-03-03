819df9df98f9678a4701f0ff5b52060103a82c3c
JoseGodoy <josemariagodoy1996@gmail.com>
2025-03-03 13:07:35 +0100

    c02-ex02

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 12:56:24 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/03 13:06:53 by unlucky          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[0] == '\0')
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z')))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	char	str[5] = "aD";
+
+	ft_putnbr(ft_str_is_alpha(str));
+	return (0);
+}
