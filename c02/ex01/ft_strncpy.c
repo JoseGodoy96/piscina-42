8000ba38ca4ca48e4474eaae94d9f948bb8fcfb8
JoseGodoy <josemariagodoy1996@gmail.com>
2025-03-03 12:55:20 +0100

    c02-ex01

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -1,36 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/02 01:46:11 by unlucky           #+#    #+#             */
-/*   Updated: 2025/03/03 12:45:30 by unlucky          ###   ########.fr       */
+/*   Created: 2025/03/03 12:44:11 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/03 12:53:57 by unlucky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	*ft_strcpy(char *dest, char *src)
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
-	while (src[i] != '\0')
+	i = 0;
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (0);
 }
 
 int	main(void)
 {
 	char	src[5] = "hola";
-	char	dest[5] = "";
+	char	dest[6] = "";
+	unsigned int	n = 6;
 
-	ft_strcpy(dest, src);
-	write(1, dest, 5);
+	ft_strncpy(dest, src, n);
+	write(1, dest, 6);
 	return (0);
 }
