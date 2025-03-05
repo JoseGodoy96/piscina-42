600bf74b5ea50d42ca18c4ccdb9dc8f7dfc529c2
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-05 18:25:35 +0100

    c05-ex03

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 15:43:05 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/05 18:24:49 by unlucky          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power > 0)
+		return (nb * ft_recursive_power(nb, power - 1));
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	int	nb = 3;
+	int	power = 2;
+
+	ft_recursive_power(nb, power);
+	ft_putnbr(ft_recursive_power(nb, power));
+	return (0);
+}
