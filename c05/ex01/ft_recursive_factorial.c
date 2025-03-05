cadfe3df42c9a2047d9aea00aed2597116ae5205
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-05 14:42:00 +0100

    c05-ex01

---
+++ c05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 13:57:00 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/05 14:39:09 by unlucky          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	int	nb = 6;
+
+	ft_recursive_factorial(nb);
+	ft_putnbr(ft_recursive_factorial(nb));
+	return (0);
+}
