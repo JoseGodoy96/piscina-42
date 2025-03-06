167f197f0f0eccdafff8454122b071a0335acc27
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-06 13:31:53 +0100

    c05-ex06

---
+++ c05/ex06/ft_is_prime.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 12:40:33 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/06 13:20:50 by unlucky          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	while (i < nb)
+	{
+		if ((nb % i) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	int	nb = 11;
+
+	ft_is_prime(nb);
+	ft_putnbr(ft_is_prime(nb));
+	return (0);
+}
