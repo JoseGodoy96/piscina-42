8e6f67fcc782b484c76a49e039346f3f5444c14c
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-07 19:18:51 +0100

    c07-ex00

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 12:36:38 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/07 19:17:57 by unlucky          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	dest = malloc(sizeof(char) * (i + 1));
+	if (dest == NULL)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	write(1, str, i);
+}
+
+int	main(void)
+{
+	char	original[] = "Hola, mundo!\n";
+	char	*copia;
+
+	copia = ft_strdup(original);
+	ft_putstr(copia);
+	free(copia);
+	return (0);
+}
