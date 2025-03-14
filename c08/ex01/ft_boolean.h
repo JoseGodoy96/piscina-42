93c27270d86d5976614e789a360b8c70e8d1dd8d
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-14 16:58:16 +0100

    c08-ex01 cambios

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -5,24 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/10 13:26:36 by unlucky           #+#    #+#             */
-/*   Updated: 2025/03/10 13:51:20 by unlucky          ###   ########.fr       */
+/*   Created: 2025/03/14 13:23:39 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/14 16:54:00 by unlucky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 
-# include <unistd.h>
-
 typedef int	t_bool;
 
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-
+# define EVEN_MSG "I have an even number of arguments"
+# define ODD_MSG "I have an odd number of arguments"
 # define TRUE 1
 # define FALSE 0
-# define EVEN(i) i % 2 == 0
-# define SUCCESS FALSE
+# define SUCCESS 0
+# define EVEN(nbr) (nbr % 2 == 0)
 
 #endif
