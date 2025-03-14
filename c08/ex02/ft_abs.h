ca6278a3879e0cc0822cd497f024750375a00c13
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-14 17:18:24 +0100

    c08-ex02

--- c08/ex02/ft_abs.h
+++ c08/ex02/ft_abs.h
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/10 12:57:47 by unlucky           #+#    #+#             */
-/*   Updated: 2025/03/13 17:22:56 by unlucky          ###   ########.fr       */
+/*   Created: 2025/03/14 16:31:31 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/14 17:17:49 by unlucky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#ifndef FT_ABS_H
+# define FT_ABS_H
 
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
+# define ABS(Value) ((Value) < 0) * -(Value) + (Value)
+
 #endif
