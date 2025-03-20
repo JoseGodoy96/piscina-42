a06c6e342ae700bf8fa2d36f8a71c2f525ebd5d3
JoseGodoy96 <josemariagodoy1996@gmail.com>
2025-03-20 14:47:55 +0100

    c11-ex01

--- c11/ex01/ft_map.c
+++ c11/ex01/ft_map.c
@@ -1,51 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: unlucky <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/19 20:28:24 by unlucky           #+#    #+#             */
-/*   Updated: 2025/03/20 13:56:45 by unlucky          ###   ########.fr       */
+/*   Created: 2025/03/20 13:53:43 by unlucky           #+#    #+#             */
+/*   Updated: 2025/03/20 14:47:45 by unlucky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
-void	ft_foreach(int *tab, int length, void (*f)(int))
+int	*ft_map(int *tab, int length, int (*f)(int))
 {
+	int	*ptr;
 	int	i;
 
+	ptr = (int *)malloc(sizeof(int) * length);
+	if (ptr == NULL)
+		return (NULL);
 	i = 0;
 	while (i < length)
 	{
-		(*f)(tab[i]);
-		write(1, " ", 1);
+		ptr[i] = (*f)(tab[i]);
 		i++;
 	}
-}
-
-void	ft_putnbr(int n)
-{
-	char	c;
-
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	c = (n % 10) + '0';
-	write(1, &c, 1);
-}
-
-int	main(void)
-{
-	int	*tab = {1, 2, 3, 4, 5};
-	int	length = 5;
-
-	ft_foreach(tab, length, &ft_putnbr);
-	return (0);
+	return (ptr);
 }
